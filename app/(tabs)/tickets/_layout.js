5e885f4fcf6a33bc983bb53b66f3fa5716ad5411
Harjot <hajotsingh2552@gmail.com>
2025-02-28 17:59:55 -0600

    Created all the passes including the  current pass that user have and other passes that user can buy, on the purchase button I added the toast

--- app/(tabs)/tickets/_layout.js
+++ app/(tabs)/tickets/_layout.js
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function AboutLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'About' }} />
+      <Stack.Screen name="index" options={{ title: 'Tickets' }} />
       <Stack.Screen name="settings" options={{ title: 'Settings' }} />
     </Stack>
   );
--- app/(tabs)/tickets/index.js
+++ app/(tabs)/tickets/index.js
@@ -1,26 +1,164 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Button,
+  ToastAndroid,
+  Pressable,
+} from "react-native";
 import { Link } from "expo-router";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function AboutScreen() {
+  const purchase = () => {
+    ToastAndroid.showWithGravity(
+      "Not Available",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  };
   return (
-    <View style={styles.container}>
-            <LinearGradient
-                    // Background Linear Gradient
-                    colors={['#0061FF', 'transparent']}
-                    style={styles.background}
-                  />
-      <Text style={styles.text}>Profile screen</Text>
-      <Link
-        href={{
-          pathname: "/tickets/settings",
-          params: { pass: false },
-        }}
-        style={styles.button}
-      >
-        Go to Settings screen
-      </Link>
-    </View>
+    <ScrollView style={styles.container}>
+      <LinearGradient
+        colors={["#0061FF", "transparent"]}
+        style={styles.background}
+      />
+      <View style={styles.box}>
+        <Text style={styles.heading}>Your Passes</Text>
+        <Pressable style={styles.yourPass}>
+          <View style={styles.flex}>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 20 },
+              ]}
+            >
+              Daily Pass
+            </Text>
+            <Ionicons name={"folder-open"} color="#000" size={24} />
+          </View>
+          <Text style={[styles.text, styles.space]}>Adult Day Pass</Text>
+          <Text style={styles.hr}></Text>
+          <Text style={[styles.text, styles.space]}>
+            Expires in{" "}
+            <Text style={styles.warning}>15 hours in 40 minutes</Text>{" "}
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.heading}>Bus Passes</Text>
+        <View style={styles.yourPass}>
+          <View style={styles.flex}>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 20 },
+              ]}
+            >
+              Day Pass
+            </Text>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 22 },
+              ]}
+            >
+              $4.99
+            </Text>
+          </View>
+          <Text
+            style={[
+              styles.text,
+              styles.space,
+              { width: "70%", lineHeight: 20, color: "#B7B7B7" },
+            ]}
+          >
+            It will be valid for 24-hours from the time you will activate it.
+          </Text>
+
+          <Text style={styles.hr}></Text>
+          <Button color="#000" onPress={() => purchase()} title="Purchase" />
+        </View>
+
+        {/* ======================================================== */}
+        <View style={styles.yourPass}>
+          <View style={styles.flex}>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 20 },
+              ]}
+            >
+              Weekly Pass
+            </Text>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 22 },
+              ]}
+            >
+              $24.99
+            </Text>
+          </View>
+          <Text
+            style={[
+              styles.text,
+              styles.space,
+              { width: "70%", lineHeight: 20, color: "#B7B7B7" },
+            ]}
+          >
+          It will be valid for 24-hours from the time you will activate it.
+          </Text>
+
+          <Text style={styles.hr}></Text>
+          <Button color="#000" onPress={() => purchase()} title="Purchase" />
+        </View>
+
+        {/* ============================================================ */}
+        <View style={styles.yourPass}>
+          <View style={styles.flex}>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 20 },
+              ]}
+            >
+              Monthly Pass
+            </Text>
+            <Text
+              style={[
+                styles.heading,
+                styles.space,
+                { color: "#000", fontSize: 22 },
+              ]}
+            >
+              $89.99
+            </Text>
+          </View>
+          <Text
+            style={[
+              styles.text,
+              styles.space,
+              { width: "70%", lineHeight: 20, color: "#B7B7B7" },
+            ]}
+          >
+         It will be valid for 24-hours from the time you will activate it.
+          </Text>
+
+          <Text style={styles.hr}></Text>
+          <Button color="#000" onPress={() => purchase()} title="Purchase" />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -28,22 +166,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#60EFFF",
-    alignItems: "center",
-    justifyContent: "center",
   },
   background: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
-    height: 700,
+    height: 800,
   },
   text: {
     color: "#000",
   },
+  heading: {
+    fontSize: 24,
+    fontWeight: 700,
+    color: "#fff",
+  },
+  box: {
+    margin: 15,
+    marginTop: 25,
+  },
+  space: {
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  hr: {
+    width: "90%", // Full width
+    height: 1, // Thin line
+    backgroundColor: "gray",
+    marginVertical: 10, // Space above and below
+  },
+  flex: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  warning: {
+    color: "red",
+  },
+  yourPass: {
+    backgroundColor: "#fff",
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+  },
   button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#000",
+    backgroundColor: "black",
   },
 });
